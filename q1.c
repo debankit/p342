dfdc399ef0d6d4767f785c508f22f8e8b1f82812
debankit <42205504+debankit@users.noreply.github.com>
2020-08-06 16:01:24 +0530

    Create q1.c

---
+++ q1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int s =0, i;
+
+    for (i = 1; i <= 100; ++i) {
+
+        // condition for non-prime
+        s=s+i;
+        
+    }
+    
+    printf("Sum of numbers from 1 to 100 = %d" , s);
+    
+
+    return 0;
+}
