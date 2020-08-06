34aaa9488c28e7893aba960a6500da9960d3d485
debankit <42205504+debankit@users.noreply.github.com>
2020-08-06 16:02:09 +0530

    Update q1.c

--- q1.c
+++ q1.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 int main() {
     int s =0, i;
-
     for (i = 1; i <= 100; ++i) {
-
         // condition for non-prime
         s=s+i;
-        
     }
-    
     printf("Sum of numbers from 1 to 100 = %d" , s);
-    
-
-    return 0;
+     return 0;
 }
