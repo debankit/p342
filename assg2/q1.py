5abe502cc09121acc0955185917d0f7a3450d186
debankit <42205504+debankit@users.noreply.github.com>
2020-08-17 14:18:12 +0530

    Update q1.py

--- assg2/q1.py
+++ assg2/q1.py
@@ -1,1 +1,8 @@
-
+#to find the average distance between 6 points in a straight line
+sum=0
+n=6
+total= n*n
+for x in range(6):
+    for y in range(6):
+        sum= sum + abs(x-y)
+print("Average distance between two points = ", sum/total)
