4e10808b8076f0655f8b899189e7a70dbebf728d
debankit <42205504+debankit@users.noreply.github.com>
2020-08-17 15:12:15 +0530

    Create q1a.c

---
+++ assg2/q1a.c
@@ -0,0 +1,13 @@
+/* to find the average distance between points in a straight line containing 6 points */
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int n = 6, total = n*n;
+    float sum = 0.0; 
+    for(int x=0; x<n; x++){
+        for(int y = 0; y<n; y++){
+            sum = sum+ abs (x-y);
+        }
+    }
+    printf("Average distance between two points = %f", sum/total);
+}
