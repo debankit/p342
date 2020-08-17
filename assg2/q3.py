8ddbae416a27f2219de6797f79bc3a434aad9355
debankit <42205504+debankit@users.noreply.github.com>
2020-08-17 21:25:39 +0530

    Update q3.py

--- assg2/q3.py
+++ assg2/q3.py
@@ -23,3 +23,6 @@ print("Product of M*N: ", p2)
 #Output
 #Product of M*A:  [50, 32, 22]
 #Product of M*N:  [[64, 338, 231], [88, 95, 102], [68, 43, 45]]
+# M= 3 22 1         N= 3 2 1
+#    4 5 6             2 15 10
+#    11 1 3            11 2 8
