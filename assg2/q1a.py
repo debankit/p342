e8a7bfbc594fe9186910b6b3a30b5d9bee443e25
debankit <42205504+debankit@users.noreply.github.com>
2020-08-17 14:52:41 +0530

    Update q1a.py

--- assg2/q1a.py
+++ assg2/q1a.py
@@ -2,7 +2,7 @@
 sum=0
 n=6
 total= n*n
-for x in range(6):
-    for y in range(6):
+for x in range(n):
+    for y in range(n):
         sum= sum + abs(x-y)
 print("Average distance between two points = ", sum/total)
