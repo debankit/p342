258a7e86f143cc60a181cb43ddd4dd508202e157
debankit <42205504+debankit@users.noreply.github.com>
2020-08-17 19:24:18 +0530

    Update q3.py

--- assg2/q3.py
+++ assg2/q3.py
@@ -5,22 +5,21 @@ with open('m.txt', 'r') as f:
 with open('n.txt', 'r') as f:
     n = [[int(num) for num in line.split()] for line in f]
 
-a= [[1, 1, 2], [1, 1, 1], [1, 1, 3]]
-p1=[[0,0,0], [0,0,0], [0,0,0]]
+a= [1, 2, 3]
+p1=[0, 0, 0]
 p2=[[0,0,0], [0,0,0], [0,0,0]]
 for x in range(3):
-    for i in range(3):
         for y in range(3):
-            p1[x][i] = p1[x][i] + (m[x][y] * a[y][i])
+            p1[x] = p1[x] + (m[x][y] * a[y])
 print("Product of M*A: ",p1)
 for x in range(3):
     for i in range(3):
         for y in range(3):
-            p2[x][i] = p1[x][i] + (m[x][y] * n[y][i]) 
+            p2[x][i] = p2[x][i] + (m[x][y] * n[y][i]) 
 print("Product of M*N: ", p2)
 
 
 
 #Output
-#Product of M*A:  [[26, 26, 31], [15, 15, 31], [15, 15, 32]]
-#Product of M*N:  [[37, 28, 39], [81, 27, 79], [48, 21, 56]]
+#Product of M*A:  [50, 32, 22]
+#Product of M*N:  [[64, 338, 231], [88, 95, 102], [68, 43, 45]]
